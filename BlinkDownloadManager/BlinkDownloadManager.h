f9e5106f1936888fef1592e6da48406a41b1326c
Mina Samy <mina.samy@blink22.com>
2024-01-24 12:42:32 +0200

    Initial project setup

---
+++ BlinkDownloadManager/BlinkDownloadManager.h
@@ -0,0 +1,18 @@
+//
+//  BlinkDownloadManager.h
+//  BlinkDownloadManager
+//
+//  Created by Mina Samy on 24/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlinkDownloadManager.
+FOUNDATION_EXPORT double BlinkDownloadManagerVersionNumber;
+
+//! Project version string for BlinkDownloadManager.
+FOUNDATION_EXPORT const unsigned char BlinkDownloadManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlinkDownloadManager/PublicHeader.h>
+
+
---
+++ BlinkDownloadManagerExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlinkDownloadManagerExamples
+//
+//  Created by Mina Samy on 24/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
